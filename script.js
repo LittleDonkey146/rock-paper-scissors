6ca801d046e62f079322cc76e5bfc8411f3ae589
littledonkey146 <alexandrosgka@gmail.com>
2022-05-07 21:02:34 +0300

    commitin

--- script.js
+++ script.js
@@ -1,52 +1,69 @@
+let computerCounter = 0;
+let playerCounter = 0;
+
+const bod = document.querySelector('body');
+const content = document.createElement('div');
+content.classList.add('content');
+bod.appendChild(content);
+
+const description = document.createElement('div');
+description.classList.add('description');
+bod.appendChild(description);
+
+const results = document.createElement('div');
+results.classList.add('results');
+bod.appendChild(results);
+
+const computerChoice = ['Rock', 'Paper', 'Scissors'];
+
+for(let i = 0; i < computerChoice.length; i++) {
+    const btn = document.createElement('button');
+    btn.classList.add('button');
+    // the player and AI will have the same choices. so take the same choices as the AI
+    btn.textContent = `${computerChoice[i]}`;
+    btn.addEventListener('click', () => playRound(btn.textContent, computerChoice));
+    content.append(btn);
+}
+
+function playerSelection(choice) {
+    console.log(choice.toLowerCase());
+    return choice.toLowerCase();
+}
+
 function ComputerPlay() {
-    const computerChoice = ['Rock', 'Paper', 'Scissors'];
-    
     return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
 function playRound(playerChoice, computerChoice) {
-    let player = prompt('What do you want to play?');
-    playerChoice = player.toLowerCase();
-
+    playerChoice = playerChoice.toLowerCase();
     computerChoice = ComputerPlay().toLowerCase(); // = the value of return from ComputerPlay() 
-
     if(playerChoice == 'paper' && computerChoice == 'scissors')
     {
         computerCounter++;
-        return alert('You lose. Paper loses to Scissors');
+        description.textContent = 'You lose. Paper loses to Scissors';
 
     } else if(playerChoice == 'scissors' && computerChoice == 'rock') {
 
         computerCounter++;
-        return alert('You lose. Scissors lose to Rock');
+        description.textContent = 'You lose. Scissors lose to Rock';
 
     } else if (playerChoice == 'rock' && computerChoice == 'paper') {
 
         computerCounter++;
-        return alert('You lose. Rock loses to Paper');
+        description.textContent = 'You lose. Rock loses to Paper';
 
     } else if(playerChoice == computerChoice) {
 
-        return alert('It\'s a tie!');
-
+        description.textContent = 'It\'s a tie!';
+    
     } else {
-        playerCounter++;
-        return alert(`You win! ${playerChoice} wins ${computerChoice}`);
-    }
-}
 
-function game() {
-    for(let i = 0; i < 5; i++) {
-        playRound();
-        scoreCounter();
+        playerCounter++;  
+        description.textContent = `You win! ${playerChoice} wins ${computerChoice}`; 
     }
+    results.textContent = scoreCounter();
 }
-
-let computerCounter = 0;
-let playerCounter = 0;
 
 function scoreCounter() {
-    return alert(`The current score is: ${playerCounter} - ${computerCounter}`);
+    return `The current score is: ${playerCounter} - ${computerCounter}`;
 }
-
-game();
