c57c9ac5cd9e813278324647d152ee78480104fd
littledonkey146 <alexandrosgka@gmail.com>
2022-05-08 17:40:05 +0300

    Made the game fully playable. Better architecture needed.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let computerCounter = 0;
 let playerCounter = 0;
 
 const bod = document.querySelector('body');
+
+const welcome = document.createElement('div');
+welcome.classList.add('welcomeMsg');
+welcome.textContent = 'Hello there! You in for playing a little, silly game? The first to 5 wins.'
+bod.appendChild(welcome);
+
 const content = document.createElement('div');
 content.classList.add('content');
 bod.appendChild(content);
@@ -14,6 +20,16 @@ const results = document.createElement('div');
 results.classList.add('results');
 bod.appendChild(results);
 
+const resetbtn = document.createElement('button');
+resetbtn.classList.add('button');
+resetbtn.textContent = 'Play Again';
+resetbtn.style.visibility = 'hidden';
+resetbtn.addEventListener('click', () => reset())
+bod.appendChild(resetbtn);
+
+function reset() {
+    window.location.reload();
+}
 const computerChoice = ['Rock', 'Paper', 'Scissors'];
 
 for(let i = 0; i < computerChoice.length; i++) {
@@ -21,13 +37,19 @@ for(let i = 0; i < computerChoice.length; i++) {
     btn.classList.add('button');
     // the player and AI will have the same choices. so take the same choices as the AI
     btn.textContent = `${computerChoice[i]}`;
-    btn.addEventListener('click', () => playRound(btn.textContent, computerChoice));
+    btn.addEventListener('click', () => decide(btn.textContent, computerChoice));
     content.append(btn);
 }
 
-function playerSelection(choice) {
-    console.log(choice.toLowerCase());
-    return choice.toLowerCase();
+// Fix it so welcome.textContent = ' ' when player decides to play.
+// Fix so when playerCounter = 5 || computerCounter = 5 'Play again' button is displayed
+// and when < 5 it is invisible and to the middle of the screen
+
+function decide(player, computer) {
+    welcome.textContent = ' ';
+    if(playerCounter < 5 && computerCounter < 5) {
+        return playRound(player, computer);
+    }
 }
 
 function ComputerPlay() {
@@ -35,33 +57,42 @@ function ComputerPlay() {
 }
 
 function playRound(playerChoice, computerChoice) {
-    playerChoice = playerChoice.toLowerCase();
-    computerChoice = ComputerPlay().toLowerCase(); // = the value of return from ComputerPlay() 
-    if(playerChoice == 'paper' && computerChoice == 'scissors')
-    {
-        computerCounter++;
-        description.textContent = 'You lose. Paper loses to Scissors';
+        
+        playerChoice.charAt(0).toUpperCase();
+        computerChoice = ComputerPlay(); // = the value of return from ComputerPlay() 
+        if(playerChoice == 'Paper' && computerChoice == 'Scissors')
+        {
+            computerCounter++;
+            description.textContent = 'You lose. Paper loses to Scissors';
 
-    } else if(playerChoice == 'scissors' && computerChoice == 'rock') {
+        } else if(playerChoice == 'Scissors' && computerChoice == 'Rock') {
 
-        computerCounter++;
-        description.textContent = 'You lose. Scissors lose to Rock';
+            computerCounter++;
+            description.textContent = 'You lose. Scissors lose to Rock';
 
-    } else if (playerChoice == 'rock' && computerChoice == 'paper') {
+        } else if (playerChoice == 'Rock' && computerChoice == 'Paper') {
 
-        computerCounter++;
-        description.textContent = 'You lose. Rock loses to Paper';
+            computerCounter++;
+            description.textContent = 'You lose. Rock loses to Paper';
 
-    } else if(playerChoice == computerChoice) {
+        } else if(playerChoice == computerChoice) {
 
-        description.textContent = 'It\'s a tie!';
-    
-    } else {
+            description.textContent = 'It\'s a tie!';
+        
+        } else {
 
-        playerCounter++;  
-        description.textContent = `You win! ${playerChoice} wins ${computerChoice}`; 
-    }
-    results.textContent = scoreCounter();
+            playerCounter++;  
+            description.textContent = `You win! ${playerChoice} wins ${computerChoice}`; 
+        }
+        if(playerCounter < 5 && computerCounter < 5) {
+            results.textContent = scoreCounter();
+        } else if(playerCounter === 5) {
+            results.textContent = `Game Over! You won the game!`;
+            resetbtn.style.visibility = 'visible';
+        } else if(computerCounter === 5) {
+            results.textContent = `Game Over! You lost the game!`;
+            resetbtn.style.visibility = 'visible';
+        }
 }
 
 function scoreCounter() {
